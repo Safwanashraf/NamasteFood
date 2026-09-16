e51ec720bf1b881ab0b1246a3166b386830f2d5d
Safwan <1.safwanashraf@gmail.com>
2026-09-16 21:42:49 +0530

    Add header layout and logo

--- App.js
+++ App.js
@@ -19,11 +19,13 @@ import ReactDOM from "react-dom/client";
         - links
 */
 
+const logo = new URL("./assets/Namaste-logo.png", import.meta.url).href;
+
 const Header = () => {
     return (
         <div className="header">
             <div>
-                <img src="./assets/Namaste-logo.png" />
+                <img className="logo" src={logo} />
             </div>
             <div className="nav-items">
                 <ul>
@@ -36,9 +38,51 @@ const Header = () => {
         </div>
     )
 };
+
+const Body = () => {
+    return (
+        <div className="body">
+            <h1>Top restaurant chains in Banglore</h1>
+            <div className="body-container">
+            <ResCard />
+            <ResCard />
+            <ResCard />
+            <ResCard />
+            <ResCard />
+            <ResCard />
+            <ResCard />
+            <ResCard />
+            <ResCard />
+            <ResCard />
+            <ResCard />
+            <ResCard />
+            <ResCard />
+            <ResCard />
+            <ResCard />
+            <ResCard />
+            <ResCard />
+            <ResCard />
+            </div>
+        </div>
+    )
+}
+
+const ResCard = () => {
+    return (
+        <div className="res-card">
+            <img className="res-card-image" src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/aouhnkmtmchfkiowm8ou"/>
+            <h3 className="res-name">Dum Safari Biryani</h3>
+            <h4 className="res-rating">3.4 . 30-40 mins</h4>
+            <h4 className="res-item">Biriyani, Hyderabadi, Kebabs, North...</h4>
+            <h4 className="res-address">Koramangala</h4>
+        </div>
+    )
+}
+
 const AppLayout = () => {
   return <div className="app">
     <Header />
+    <Body />
   </div>;
 };
 
