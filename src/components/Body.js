f56797c39f6bf99a23f16f669cfdcc77be9e00a2
Safwan <1.safwanashraf@gmail.com>
2026-09-21 18:25:21 +0530

    Created Top Rated Restuarants list using useState and filterfunction

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,13 +1,20 @@
 import RestaurantCard from "./RestaurantCard";
 import resData from "../utils/mockData"; 
+import { useState } from "react";
 
 const Body = () => {
+
+    const [Restaurants, setRestaurants] = useState(resData);
     return (
         <div className="body">
+            <button className="btn-top-rated" onClick={ () =>{
+                setRestaurants(Restaurants.filter(res => res.info.avgRating > 4.5))
+            }}
+            >Top Rated Restaurants</button>
             <h1>Top restaurant chains in Banglore</h1>
             <div className="body-container">
                 {
-                    resData.map((Restaurant) => (<RestaurantCard key={Restaurant.info.id} resData={Restaurant} />))
+                    Restaurants.map((res) => (<RestaurantCard key={res.info.id} resData={res} />))
                 }
             </div>
         </div>
