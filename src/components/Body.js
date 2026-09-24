67b5bfd3c07ef3a44b23db2a9eb63ed960e43f57
Safwan <1.safwanashraf@gmail.com>
2026-09-24 20:06:54 +0530

    Fetch restaurant data in body

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,10 +1,26 @@
 import RestaurantCard from "./RestaurantCard";
 import resData from "../utils/mockData"; 
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const Body = () => {
-
+    // Local State Variable - Super powerful variable.
     const [Restaurants, setRestaurants] = useState(resData);
+
+    useEffect(() => {
+        fetchData();
+    }, [])
+
+    const fetchData = async () => {
+        const data = await fetch(
+            "https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9352403&lng=77.624532&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+        );
+
+        const json = await data.json();
+        setRestaurants(json.data.cards);
+        console.log(json);
+
+    }
+
     return (
         <div className="body">
             <button className="btn-top-rated" onClick={ () =>{
--- src/components/testing.js
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-export const testing = () =>{
-    console.log("hello")
-}
